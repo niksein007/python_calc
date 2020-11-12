6be0f4118d0e811dc3fd57ca6d28c9336d5423d0
niksein007 <markpearceniksein@gmail.com>
2020-11-12 18:10:27 +0000

    major logic done

--- test.py
+++ test.py
@@ -3,45 +3,48 @@ from browser import document , alert, html
 calculator = document['calculator']
 
 display = html.DIV('',id='display')#create div to display result
-list = '0123456789-+/*=c' #make a string list of cal buttons
+list = ['.','0','1','2','3','4','5','6','7','8','9','-','+','/','*','=','c','Del'] #make a string list of cal buttons
 button_list = [display] #put display first to aviod the btns showing as elements
 
 
 ###creating the logic function
-firstvals = []
-secondvals = []
 def logic(ev):
    """ compare the buttons """
-   global firstvals
-   global secondvals
    btn_value = ev.target.text## holds the content of the clicked btn
-   symbols = '+-/*'## holds all possible math symbols
+   symbols = '+-/*='## holds all possible math symbols
    len_symbols = len(symbols)
-   count = 0 ###used to prevent duplication when iteration is run
+   count = 0
+   msg1 = 'please enter an expression'
 
 
    if btn_value == 'c':
       display.text = ''
-      firstvals = []
-      secondvals = []
 
+   elif btn_value == 'Del':
+     display.text = display.text[:-1] ### slice off all chars except the last char
+
+      
    elif btn_value == '=':
-      sum_vals = eval(display.text)
-      display.text = sum_vals
+      if (display.text == '') or (display.text == msg1) \
+      or (display.text == '*')  or (display.text == '-') \
+      or (display.text == '/')  or (display.text == '+')   \
+      :
+         display.text = msg1
+      else:
+         sum_vals = eval(display.text)
+         display.text = sum_vals
    else:
       for symbol in symbols:
-         if symbol in display.text:
-            display.text += btn_value
-            secondvals.append(btn_value)
+         if (btn_value == symbol) and (symbol in display.text):
+            pass
          else:
             count += 1
-            if count == len_symbols:
+            if count == len_symbols:###update display after all symbol checks are done
+               if display.text == msg1:
+                  display.text = ''
                display.text += btn_value
-               firstvals.append(btn_value)
-            
-   print(f'first {firstvals}')
-   print(f'second {secondvals}')
-   print(count)
+           
+  
 
 
 
